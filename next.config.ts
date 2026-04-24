674a59057f87a9034d8b42645d652cb9481fafa3
csalguera <carlos.e.salguera@gmail.com>
2026-04-23 22:08:24 -0400

    add S3 image host to Next.js image config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.fabtcg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
